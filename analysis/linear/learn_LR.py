0e7cf4ac684bfcebec765c17e632dc3f32237644
bird880702 <51665796+bird880702@users.noreply.github.com>
2020-12-10 14:54:27 +0800

    Add files via upload

--- analysis/linear/learn_LR.py
+++ analysis/linear/learn_LR.py
@@ -7,46 +7,41 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+
 
 
 txt = ".\human.txt"
 hm = np.loadtxt(txt, delimiter=',')
 mv = np.loadtxt('.\move.txt', delimiter=',')
 al = np.loadtxt('alert.txt', delimiter=',')
-m = (mv[:,:2])
+a = (al[:,:2])
 h = (hm[:,:2])
-a = np.vstack([m,h])
+a = np.vstack([h, a])
 b = np.ones(500)
 b = np.concatenate([b, b*(-1)])
 c = ['hr', 'br']
 x = pd.DataFrame(a, columns=c)
-y = pd.DataFrame(b, columns=['class'])
+y = pd.DataFrame(b, columns=['target'])
 data = pd.concat([x,y], axis=1)
 
-
 X_train, X_test, y_train, y_test = train_test_split(
-    data[['hr','br']], data[['class']], test_size=0.3, random_state=0)
+    data[['hr','br']], data[['target']], test_size=0.3, random_state=0)
 
 sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
-
-
 lr = LogisticRegression()
-lr.fit(X_train_std,y_train['class'].values)
-LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-          intercept_scaling=1, max_iter=100, multi_class='ovr', n_jobs=1,
-          penalty='l2', random_state=None, solver='liblinear', tol=0.0001,
-          verbose=0, warm_start=False)
+lr.fit(X_train_std,y_train['target'].values)
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('blue', 'orange', 'lightgreen', 'gray', 'cyan')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     # plot the decision surface
@@ -86,7 +81,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                     linewidths=1,
                     marker='o',
                     s=55, label='test set')
-plot_decision_regions(X_train_std, y_train['class'].values, classifier=lr)
+plot_decision_regions(X_train_std, y_train['target'].values, classifier=lr)
 plt.xlabel('br')
 plt.ylabel('hr')
 plt.legend(loc='upper left')
